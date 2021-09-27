3feed45f5354b57a4cee4b7bc25a4702296b728b
Adonaiis <76216176+yoann-f@users.noreply.github.com>
2021-09-27 20:24:15 +0200

    Update #1

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,54 @@
 /*FICHIER CONTENANT L'ENSEMBLE DES FONCTIONS JAVASCRIPT DU SITE*/
+
+
+/*FONCTION PERMETTANT DE RÉCUPÉRER L'ID PRODUIT*/
+getProductID = (id) => {
+    //A l'aide de la propriété "search", de l'interface "location", j'isole une chaîne de requête contenant un "?", suivi de paramètres URL
+    const urlPage = window.location.search;
+
+    //Une fois le paramètre URL isolé, je récupère uniquement l'élément qui m'intéresse, dans mon cas, l'ID du produit présent dans l'URL
+    const urlProductPage = new URLSearchParams(urlPage);
+
+    //Récupération de l'ID du produit présenté sur la page
+    idProduct = urlProductPage.get("id");    
+
+    //Met fin à l'éxécution de la fonction et définit une valeur à renvoyer à la fonction appelante
+    return idProduct; 
+};
+
+
+/*FONCTION PERMETTANT LA RÉCUPÉRATION DE LA QUANTITÉ DE PRODUITS PRÉSENTS DANS LE PANIER*/
+/*Je récupère la quantité de produit ajouté au panier et l'affiche dans le panier en page d'accueil*/
+getProductQTY = (qty) => {
+    var myqty = document.getElementById("qty-cart");
+    var qty = localStorage.getItem("cartQTY");
+    myqty.innerText = qty;
+};
+
+
+/*FONCTION DE RÉCUPÉRATION DE LA COULEUR DU PRODUIT AJOUTÉ AU PANIER*/
+getProductColor = () => {
+
+};
+
+
+/*FONCTION PERMETTANT DE MANIPULER LE PANIER*/
+//Vérifie la liste des produits présents dans le panier
+checkCartProductList = () => {
+
+};
+
+//Vérifie la présence d'un produit dans le panier
+checkCartProductItem = () => {
+
+};
+
+//Ajoute un produit au panier
+addProductToCart = () => {
+
+};
+
+//Supprime un produit du panier
+deleteProductFromCart = () => {
+
+};
--- js/variables.js
+++ js/variables.js
@@ -1,1 +1,28 @@
 /*FICHIER CONTENANT L'ENSEMBLE DES VARIABLES JAVASCRIPT DU SITE*/
+
+
+/*VARIABLES DONT LA VALEUR RESTE CONSTANTE*/
+    //Déclaration de l'url de l'API en variable
+    const URL_API = "https://teddies-api.herokuapp.com/api/";
+
+    //Déclaration en variable du type de produit vendu dans la boutique. Options possibles : "cameras", "teddies" ou "furniture"
+    const productType = "teddies";
+
+
+/*VARIABLES DONT LA VALEUR EST RÉINITIALISÉE A CHAQUE CHARGEMENT DE PAGE*/
+    //Déclaration de la variable lsqty qui permet de remettre à zéro la quantité
+    var lsqty = 0;
+
+
+/*VARIABLES A VALEUR ÉVOLUTIVE*/
+    var cardQTY = localStorage.cardQTY;
+    var cardList;
+
+    // Variables pour le panier
+
+
+
+    var productItem = {}; //Je déclare un OBJET: on récupère les valeurs product_ID, product_QTY et product_COLOR
+    var product_ID; //String de l'url produit
+    var product_QTY; //Integer: on récupère la quanttié de produit
+    var product_COLOR; //String de la couleur du produit
